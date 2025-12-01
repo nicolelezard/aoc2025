8f923bc25b133f81eeb7c986d0e9e2dc9050061d
nicolas.martineau <nmartineau@ourmagnet.net>
2025-12-01 21:22:48 +0100

    day 1 part 1 done

--- 1/1.ps1
+++ 1/1.ps1
@@ -2,5 +2,26 @@
 param (
 )
 
-$rawInput = Get-Content $PSScriptRoot\example.txt
-# $rawInput = Get-Content $PSScriptRoot\input.txt
+$ErrorActionPreference = 'Stop'
+
+# $rawInput = Get-Content $PSScriptRoot\example.txt
+$rawInput = Get-Content $PSScriptRoot\input.txt
+
+<# The actual password is the number of times the dial is left pointing at 0 after any rotation in the sequence.
+Because the dial points at 0 a total of three times during this process, the password in this example is 3.
+Analyze the rotations in your attached document. What's the actual password to open the door? #>
+
+$start = 50
+$turns = $rawInput -replace 'L', '-' -replace 'R', '' | % {[int]$_}
+
+$current = $start
+$hits = 0
+foreach ($turn in $turns) {
+    $current += $turn
+
+    if ($current % 100 -eq 0) {
+        $hits++
+    }
+}
+
+"The password is $hits"
