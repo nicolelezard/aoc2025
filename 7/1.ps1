a765e8c4e00ef992d918809eaec91a6effb8579f
nicolas.martineau <nmartineau@ourmagnet.net>
2025-12-09 17:50:54 +0100

    day 7 part 1 done

--- 7/1.ps1
+++ 7/1.ps1
@@ -2,8 +2,83 @@
 param (
 )
 
-$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Break'
 . $PSScriptRoot\..\utils\*.ps1
 
-$rawInput = Get-Content $PSScriptRoot\example.txt
-# $rawInput = Get-Content $PSScriptRoot\input.txt
+# $rawInput = Get-Content $PSScriptRoot\example.txt
+$rawInput = Get-Content $PSScriptRoot\input.txt
+
+$grid = ,$rawInput | Read-AsGrid -of {
+    @{
+        Value = $_
+        IsSplitter = '^' -eq $_
+        IsBeam = 'S' -eq $_
+    }
+}
+
+
+function Split-TachyonRay {
+    [CmdletBinding()]
+    param (
+        # 2D grid of objects (hastables)
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [hashtable[,]]$fromGrid
+    )
+
+    $f = $fromGrid
+    $splitCount = 0
+    
+    $yBound = $f.GetUpperBound(0)
+    $xBound = $f.GetUpperBound(1)
+    $g = [hashtable[,]]::new($yBound +1, $xBound +1)
+    [hashtable[,]]::Copy($f, 0, $g, 0, $f.Count)
+    
+    :y for ($y = 0; $y -lt $yBound; $y++) {
+
+        :x for ($x = 0; $x -le $xBound; $x++) {
+
+            $pos = $g[$y,$x]
+
+            if ($pos.IsSplitter) {
+                continue x
+            }
+
+            # find the rays
+            if ($pos.IsBeam) {
+                
+                $next = $g[($y+1),$x]
+
+                # will it hit a splitter?
+                if ($next.IsSplitter) {
+                    # split the beam
+                    $left = $g[($y+1),($x-1)]
+                    $right = $g[($y+1),($x+1)]
+                    
+                    $left.IsBeam = $true
+                    $left.Value = '|'
+                    $right.IsBeam = $true
+                    $right.Value = '|'
+
+                    $splitCount++
+                }
+                else {
+                    # draw a beam under it
+                    $next.IsBeam = $true
+                    $next.Value = '|'
+                }
+            }
+        }
+    }
+
+    Write-Output -NoEnumerate $g
+    Write-Output $splitCount
+}
+
+$newGrid, $splitCount = ,$grid | Split-TachyonRay
+
+,$newGrid | Show-Grid -of {
+    $_.Value
+}
+
+""
+"The tachyon ray was split $splitCount times"
